11799e5c7e06ca78a7ce97f2b539faf6115932e5
Nathaniel <nathanielbughar@outlook.com>
2022-03-28 23:20:12 +0100

    master

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,9 @@
+from distutils.command.config import config
+from brownie import network, accounts, config
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
